1f3e955d29f7ed88fb5aa0d87ebeaa69b6cb9c2d
aziyan99 <rajaazian08@gmail.com>
2022-01-31 01:35:30 +0700

    update users column

---
+++ database/migrations/2022_01_30_183317_update_users_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class UpdateUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->string('avatar')->nullable();
+            $table->string('phone_number');
+            $table->string('address');
+            $table->enum('role', ['admin', 'user'])->default('user');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('avatar');
+            $table->dropColumn('phone_number');
+            $table->dropColumn('address');
+            $table->dropColumn('role');
+        });
+    }
+}
