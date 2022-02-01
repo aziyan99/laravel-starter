3c68ff002f91c36da2b30cd7a19439379ff8a2dd
aziyan99 <rajaazian08@gmail.com>
2022-02-01 23:20:01 +0700

    update ui

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,5 +1,7 @@
 @extends('layouts.auth')
 
+@section('title', 'Update password')
+
 @section('main')
 <div class="container">
     <div class="row justify-content-center">
--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,9 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Auth::routes();
-
-Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+Auth::routes([
+    'register' => false
+]);
 
 Route::group(['prefix' => 'admin', 'middleware' => ['auth']], function () {
     Route::get('/dashboard', [DashboardController::class, 'index'])->name('dashboard.index');
